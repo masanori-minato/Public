f768b0518ec17810b6f12c9c5584f0be4f0af4af
Minato <masanori_minato@yahoo.co.jp>
2026-05-09 08:53:03 +0900

    いろいろ

--- VBA/todays_work_memo.vb
+++ VBA/todays_work_memo.vb
@@ -3,11 +3,11 @@ Sub CreateTodaysWorkMemo()
     Dim fileName As String
     Dim savePath As String
 
-    fileName = "本日の作業メモ_" & Format(Date, "YYYYMMDD") & ".xlsm"
+    fileName = "本日の作業メモ_" & Format(Date, "YYYYMMDD") & ".xlsx"
     savePath = Environ("USERPROFILE") & "\Downloads\" & fileName
 
     Set wb = Workbooks.Add
-    wb.SaveAs fileName:=savePath, FileFormat:=xlOpenXMLWorkbookMacroEnabled
+    wb.SaveAs fileName:=savePath, FileFormat:=xlOpenXMLWorkbook
 
     MsgBox "作成しました：" & fileName
 End Sub
